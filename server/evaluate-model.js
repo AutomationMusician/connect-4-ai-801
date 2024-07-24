ef43bdd83e573ff60955d95ed339c996fa81d6ee
joshuafishman <josh.a.fishman@gmail.com>
2024-07-24 00:19:06 -0400

    evaluation works!

--- server/evaluate-model.js
+++ server/evaluate-model.js
@@ -1,26 +1,28 @@
 import { GameBoard, emptyBoardXOFormat, filpXO }  from 'common/game';
 
-async function playGame() {
+async function playGame(startingPlayer, difficulty) {
     const gameBoard = new GameBoard(emptyBoardXOFormat); // create blank board
     let gameStatus = 'U';
-
-    const players = ['X', 'O']; 
-    const nextMoveFunction = {
-        'X': getThisModelMove,
-        'O': getOtherModelMove
-    }
-    let currentPlayerIndex = 0;
+    let currentPlayer = startingPlayer;
 
     while (gameStatus === 'U')
     {
-        const currentPlayer = players[currentPlayerIndex];
-        const move = await nextMoveFunction[currentPlayer](gameBoard.toXOFormat());
+        let move;
+        let nextPlayer;
+        if (currentPlayer === 'X') {
+            move = await getThisModelMove(gameBoard.toXOFormat());
+            nextPlayer = 'O';
+        }
+        else {
+            move = await getOtherModelMove(gameBoard.toXOFormat(), difficulty);
+            nextPlayer = 'X';
+        }
         gameBoard.place(move, currentPlayer);
         gameStatus = gameBoard.status();
-        console.log(`${currentPlayer} played in column ${move}. Game board is now: ${gameBoard.toXOFormat()}`);
-        currentPlayerIndex = (currentPlayerIndex + 1) % players.length; // increment or go back to beginning if we are at the end
+        // console.log(`${currentPlayer} played in column ${move}. Game board is now: ${gameBoard.toXOFormat()}`);
+        currentPlayer = nextPlayer;
     }
-    console.log("Game status = " + gameStatus);
+    return gameStatus;
 }
 
 
@@ -33,9 +35,9 @@ async function getThisModelMove(xoformat) {
     return Number(responseText);
 }
 
-async function getOtherModelMove(xoformat) {
+async function getOtherModelMove(xoformat, difficulty) {
     xoformat = filpXO(xoformat);
-    const response = await fetch(`https://tleemann.de/php/four.php?move=${xoformat}&level=hard`);
+    const response = await fetch(`https://tleemann.de/php/four.php?move=${xoformat}&level=${difficulty}`);
     if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
     }
@@ -43,4 +45,43 @@ async function getOtherModelMove(xoformat) {
     return Number(responseText);
 }
 
-playGame();
+
+// const difficulties = ['easy', 'hard', 'pro'];
+const difficulties = ['easy', 'hard'];
+const players = ['X', 'O'];
+const numGamesPerScenario = 20;
+
+// asynchronously start all games
+const gameStatusPromises = [];
+difficulties.forEach(difficulty => {
+    players.forEach(startingPlayer => {
+        for (let i=0; i<numGamesPerScenario; i++) {
+            gameStatusPromises.push(playGame(startingPlayer,difficulty));
+        }
+    });
+});
+
+// wait for games to complete and then output the results
+Promise.all(gameStatusPromises).then(gameStatuses => {
+    const outputTable = [];
+    let index = 0;
+    difficulties.forEach(difficulty => {
+        players.forEach(startingPlayer => {
+
+            const row = {
+                difficulty,
+                startingPlayer,
+                'X': 0,
+                'O': 0,
+                'T': 0
+            };
+            for (let i=0; i<numGamesPerScenario; i++) {
+                const result = gameStatuses[index];
+                row[result]++;
+                index++;
+            }
+            outputTable.push(row);
+        });
+    });
+    console.table(outputTable);
+});
