f4e05db832dbadf3488f7dbae1b9ab1f81f4eb99
Thangarasu Dhanabal <84553906+rsdtinfo01@users.noreply.github.com>
2024-07-27 12:10:09 -0400

    Implements the minimax algorithm with alpha-beta pruning to determine the best move. (#5)
    Co-authored-by: joshuafishman <josh.a.fishman@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,31 +1,41 @@
 import express from 'express';
-import * as path from 'path';
-const __dirname = import.meta.dirname; // need to define __dirname because we are using ES Modules rather than CommonJS
-import { GameBoard, numWide }  from 'common/game';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import { GameBoard } from '../common/game.mjs';
+import { minimax } from './minimax.js';
+// Define __dirname using ES module syntax
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+app.use(bodyParser.json());
+// CORS option setup
+const corsOptions = {
+    origin: '*', 
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Handle preflight requests
+
 const port = process.env.PORT || 3000;
 
-// redirect root to '/client' route
+// Redirect root to '/client' route
 app.get('/', (req, res) => res.redirect('/client'));
 
 app.use('/client', express.static(path.join(__dirname, '../client')));
-
 app.use('/common', express.static(path.join(__dirname, '../common')));
 
 app.get('/api/next-move/:xoformat', (req, res) => {
-    const game = new GameBoard(req.params.xoformat);
-    if (game.status() !== 'U') {
-        res.status(400).send("Game is complete. No move can be made");
-        return;
-    }
-    let randomColumn;
-    do {
-        randomColumn = Math.floor(Math.random() * numWide); // return random number 0-6.
-    } while(game.columnFull(randomColumn));
-    console.log(`Received game state ${req.params.xoformat}. The AI choses to move in column index ${randomColumn}`);
-    setTimeout(() => res.send(String(randomColumn)), 1000); // send column as a response. Use setTimeout to simulate it taking a full second.
+    const gameBoard = new GameBoard(req.params.xoformat);
+    const bestCol = minimax(gameBoard); 
+    console.log(`Received game state ${req.params.xoformat}. The AI chooses to move in column index ${bestCol}`);
+    res.send(String(bestCol)); // Send column as a response. Use setTimeout to simulate it taking a full second.
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ server/minimax.js
@@ -0,0 +1,105 @@
+import { GameBoard } from '../common/game.mjs';
+
+const MAX_DEPTH = 7;
+
+/**
+ * Implements the minimax algorithm with alpha-beta pruning to determine the best move.
+ * See "Artificial Intelligence - A Modern Approach (3rd Edition)" page 170.
+ *
+ * @param {GameBoard} gameBoard - The current state of the Connect4 game board.
+ * @returns {integer} - The best column to play
+ */
+export function minimax(gameBoard) {
+    const availableColumns = gameBoard.availableColumns();
+    let bestValue = -Infinity;
+    let bestCols;
+    availableColumns.forEach(col => {
+        gameBoard.place(col, 'X'); // update board
+        const value = minimaxRecursive(gameBoard, 0, -Infinity, Infinity, 'O');
+        if (value > bestValue) {
+            bestValue = value;
+            bestCols = [col];
+        } 
+        else if (value === bestValue) {
+            bestCols.push(col);
+        }
+        gameBoard.remove(col); // undo board update
+    });
+    return bestCols[Math.floor(Math.random() * bestCols.length)]; // return random value from the best column
+}
+
+/**
+ * Implements the minimax algorithm with alpha-beta pruning to determine the best move.
+ * 'X' (the AI) is the maximizing player and 'O' the opponent is the minimizing player
+ *
+ * @param {GameBoard} gameBoard - The current state of the Connect4 game board.
+ * @param {number} depth - The maximum depth of the search tree.
+ * @param {number} alpha - The alpha value for alpha-beta pruning.
+ * @param {number} beta - The beta value for alpha-beta pruning.
+ * @param {character} player - character representation of the player.
+ * @returns {number} - score
+ */
+function minimaxRecursive(gameBoard, depth, alpha, beta, player) {
+    const status = gameBoard.status();
+
+    // check if there is a winner or it is a tie
+    if (status !== 'U')
+    {
+        let value = 0; // default to tie
+        if (status === 'O') {
+            value = -1; 
+        }
+        else if (status === 'X') {
+            value = 1;
+        }
+        // make the value more extreme (a bigger win/loss) if it is shallower in the search tree 
+        // and less extreme (a smaller win/loss) if it is deeper in the search tree
+        return value * (MAX_DEPTH - depth + 1);
+    }
+
+    // check if we've reached the maximum depth
+    if (depth === MAX_DEPTH) {
+        // TODO: add evaluation heuristic
+        return 0;
+    }
+
+    const availableCols = gameBoard.availableColumns();
+    if (player === 'X') {
+        // maximizing player
+        let value = -Infinity;
+        for (let col of availableCols) {
+            gameBoard.place(col, 'X'); // update board
+            const newScore = minimaxRecursive(gameBoard, depth + 1, alpha, beta, 'O');
+            // console.log(`${gameBoard.toXOFormat()} - score: ${newScore}`);
+            gameBoard.remove(col); // undo update
+            if (newScore > value) {
+                value = newScore;
+            }
+            if (value >= beta) {
+                return value;
+            }
+            if (value > alpha) {
+                alpha = value;
+            }
+        }
+        return value;
+    } else {
+        let value = Infinity;
+        for (let col of availableCols) {
+            gameBoard.place(col, 'O'); // update board
+            const newScore = minimaxRecursive(gameBoard, depth + 1, alpha, beta, 'X');
+            // console.log(`${gameBoard.toXOFormat()} - score: ${newScore}`);
+            gameBoard.remove(col); // undo update
+            if (newScore < value) {
+                value = newScore;
+            }
+            if (value <= alpha) {
+                return value;
+            }
+            if (value < beta) {
+                beta = value;
+            }
+        }
+        return value;
+    }
+}
